738e0d80d08766db6e369e50e991fecd603e8665
Kevin Cripe <kevincripe6@gmail.com>
2024-10-19 11:25:59 -0500

    css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
-import React, {useState} from 'react';
 import { TodoMain } from './components/TodoMain';
 
 
 const App = () => {
 	
-	const [visible, setVisible] = useState(false)
 
 	return (
 	  <>
-		<header>
-			<h1>{ visible ? null : 'FCC ToDo' }</h1>
+		<header className='header'>
+			<h1>FCC ToDo</h1>
 		</header>
 		<TodoMain />
 	  </>
--- src/components/TodoMain.js
+++ src/components/TodoMain.js
@@ -1,11 +1,14 @@
 import {todoListAtom} from '../recoil/atoms/todoAtom';
 import { useRecoilValue } from 'recoil';
 import { TodoItemCreator } from './TodoItemCreator';
+import './todo.css'
 
 export const TodoMain = () => {
+
     const todoList = useRecoilValue(todoListAtom)
+    
     return (
-        <div>
+        <div className='parent-container'>
             <TodoItemCreator/>
             <div>
                 {console.log(todoList)}
