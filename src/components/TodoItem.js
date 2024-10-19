36bb8a686d2333a06a9d8f362830880af2200e29
Kevin Cripe <kevincripe6@gmail.com>
2024-10-19 15:35:15 -0500

    index

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,19 @@
+import { useRecoilState } from "recoil";
+import { todoListAtom } from "../recoil/atoms/todoAtom";
 
 
-export const TodoItem = ({item}) => {
+export const TodoItem = ({ item }) => {
+
+    const [todoList, setTodoList] = useRecoilState(todoListAtom)
+    const index = todoList.findIndex((listItem) => listItem === item)
+    
 
     return (
         <div>
-        <input 
-        type="text"
-        value={item.text} 
-        />
+            <input
+                type="text"
+                value={item.text}
+            />
         </div>
     )
 }
