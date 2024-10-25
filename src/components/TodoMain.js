b1cf114a655e8711de794c6eb7766a33316b31bf
Kevin Cripe <kevincripe6@gmail.com>
2024-10-24 21:19:35 -0500

    filtered todo

--- src/components/TodoMain.js
+++ src/components/TodoMain.js
@@ -1,29 +1,28 @@
 import { todoListAtom } from '../recoil/atoms/todoAtom';
 import { useRecoilValue } from 'recoil';
 import { TodoItemCreator } from './TodoItemCreator';
-import {TodoItem} from './TodoItem';
+import { TodoItem } from './TodoItem';
 import FilterDropdown from './FilterDropDown';
 import './todo.css'
 import { filteredTodoListSelector } from '../recoil/selectors/filteredTodoSelector';
 
 export const TodoMain = () => {
 
-    const todoList = useRecoilValue(todoListAtom)
     const filteredTodoList = useRecoilValue(filteredTodoListSelector)
 
     return (
         <div className='parent-container'>
             <div>
-            <TodoItemCreator />
-            <FilterDropdown />
-                {todoList.length > 0 && (
+                <TodoItemCreator />
+                <FilterDropdown />
+                {filteredTodoList.length > 0 ? (
                     <div style={{ color: 'white' }} className='todos-list'>
                         {filteredTodoList.map((filterItem) => (
                             <TodoItem key={filterItem.id} item={filterItem} />
-                            ))}
+                        ))}
                     </div>
-                )}
-            </div>
+                ) : (<div style={{ color: 'white' }}>You have not completed any tasks...</div>)}
+                </div>
         </div>
-    )
-}
+            );
+};
