d30e0eac47dc5fcfef37781b868307d76099b93b
Kevin Cripe <kevincripe6@gmail.com>
2024-10-24 21:26:27 -0500

    css

--- src/components/FilterDropdown.js
+++ src/components/FilterDropdown.js
@@ -1,5 +1,6 @@
 import {useRecoilState} from 'recoil';
 import {filterOptionAtom} from '../recoil/atoms/filterOptionAtom';
+import './todo.css'
 
 const FilterDropdown = () => {
     const [filterOption, setFilterOption] = useRecoilState(filterOptionAtom);
@@ -9,8 +10,8 @@ const FilterDropdown = () => {
     }
 
     return (
-        <div style={{ color: 'white' }}>
-            <span>Filter Todos </span>
+        <div className='filter-dropdown'>
+            <span className='filter-text'>Filter Todos:</span>
                 <select value={filterOption} onChange={handleFilterChange}>
                     <option value="all">All</option>
                     <option value="completed">Completed</option>
